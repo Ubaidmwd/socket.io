e0e45ea4c532c2440f72ac0786e300e7d72d807a
Ubaidmwd <ubaidmwd@gmail.com>
2023-10-29 11:25:37 +0500

    event of

--- index.js
+++ index.js
@@ -7,16 +7,24 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-io.on("connection", (socket) => {
+// io.on("connection", (socket) => {
+//   console.log("user connected");
+//   setTimeout(() => {
+// socket.send("Hello World");
+// socket.emit("message", { description: "My event description" });
+//   }, 2000);
+let activeUsers = 0;
+let myName = io.of("/mysapce");
+myName.on("connection", (socket) => {
+  activeUsers++;
   console.log("user connected");
-  setTimeout(() => {
-    // socket.send("Hello World");
-    socket.emit("message", { description: "My event description" });
-  }, 2000);
+  socket.emit("message", activeUsers);
+
   socket.on("disconnect", function () {
     console.log("user disconnected");
   });
 });
+
 server.listen(3000, function () {
   console.log(`Listening on port ${3000}`);
 });
